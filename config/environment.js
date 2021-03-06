1b78b96362e32ae189d8c3658444b65a93b0b14f
Hugo Kinahan <hugokinahan@hotmail.com>
2021-03-06 18:03:51 +0000

    some changes

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,6 @@
-export const dbURI = 'mongodb://localhost/la-cruche-db'
-export const port = 4000
-export const secret = 'la cruche 2020'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/la-cruche-db'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'la cruche 2020'
